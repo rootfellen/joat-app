da6a02f045351435f711fe982310d97aefb11068
serhii <rootfellenua@gmail.com>
2022-08-17 20:39:52 -0400

    add styles for regex util

--- src/pages/RegExHelper/RegexHelper.jsx
+++ src/pages/RegExHelper/RegexHelper.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Container } from "./RegexHelperElements";
 
 const RegexHelper = () => {
-  return <div>Regex Helper</div>;
+  return <Container>Regex Helper</Container>;
 };
 
 export default RegexHelper;
