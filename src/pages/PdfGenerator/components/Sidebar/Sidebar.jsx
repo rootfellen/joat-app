4c07033202a327030bf1105a6a0ced023b1cf5f2
serhii <rootfellenua@gmail.com>
2022-10-22 18:18:47 -0700

    adds select, textarea inputs

--- src/pages/PdfGenerator/components/Sidebar/Sidebar.jsx
+++ src/pages/PdfGenerator/components/Sidebar/Sidebar.jsx
@@ -1,39 +1,113 @@
+import {
+  Chip,
+  FormControl,
+  Input,
+  InputLabel,
+  MenuItem,
+  OutlinedInput,
+  Select,
+} from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
 import { useState } from "react";
-import { Label, Wrapper, Template, Select } from "./SidebarElements";
+import { Wrapper } from "./SidebarElements";
 
 const Sidebar = () => {
   const [data, setData] = useState({
+    postage: "",
+    sections: [],
     template: "",
   });
 
+  const sections = [
+    "Employee Address",
+    "Contractor Signature",
+    "Contractor Address",
+    "Title",
+  ];
+
   const selectHandler = (e) => {
     setData((prevState) => {
+      const { name, value, type, checked } = e.target;
       return {
         ...prevState,
-        template: e.target.value,
+        [name]: type === "checkbox" ? checked : value,
       };
     });
   };
   console.log(data);
   return (
     <Wrapper>
-      Sidebar
-      <Template>
-        <Label htmlFor="templates">Document template:</Label>
+      <Message></Message>
+      <FormControl fullWidth style={{ margin: "1rem 0rem" }}>
+        <InputLabel id="templates">Document template</InputLabel>
         <Select
-          onChange={selectHandler}
-          id="templates"
+          labelId="templates"
+          id="document-templates"
           value={data.template}
           name="template"
+          label="Document template"
+          onChange={selectHandler}
         >
-          <option value="">Select predefined template</option>
-          <option value="invoice">Invoice</option>
-          <option value="cover Letter">Cover Letter</option>
-          <option value="recommendation Letter">Recommendation Letter</option>
-          <option value="certificate">Certificate</option>
+          <MenuItem value={"Invoice"}>Invoice</MenuItem>
+          <MenuItem value={"Letter"}>Cover Letter</MenuItem>
+          <MenuItem value={"Agreement"}>Agreement</MenuItem>
+          <MenuItem value={"Recommendation"}>Recommendation</MenuItem>
+          <MenuItem value={"Certificate"}>Certificate</MenuItem>
         </Select>
-      </Template>
+      </FormControl>
+      <div style={{ display: "flex", justifyContent: "space-between" }}>
+        <FormControl style={{ width: "48%" }}>
+          <InputLabel id="sections">Document Sections</InputLabel>
+          <Select
+            labelId="sections"
+            id="document-sections"
+            multiple
+            value={data.sections}
+            name="sections"
+            onChange={selectHandler}
+            input={
+              <OutlinedInput
+                id="select-multiple-chip"
+                label="Document Sections"
+              />
+            }
+            renderValue={(selected) => (
+              <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                {selected.map((value) => (
+                  <Chip
+                    key={value}
+                    label={value}
+                    style={{ backgroundColor: "#31403c", color: "#fff" }}
+                  />
+                ))}
+              </Box>
+            )}
+          >
+            {sections.map((section) => (
+              <MenuItem key={section} value={section} style={{}}>
+                {section}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl style={{ width: "48%" }}>
+          <InputLabel
+            style={{ margin: "0.7rem 0rem 0.2rem 0rem" }}
+            shrink
+            id="postage"
+          >
+            Postage of document
+          </InputLabel>
+          <OutlinedInput
+            fullWidth
+            type="date"
+            onChange={selectHandler}
+            name="postage"
+          />
+        </FormControl>
+      </div>
+      <textarea style={{ height: "60%", marginTop: "1.125rem" }}></textarea>
     </Wrapper>
   );
 };
--- src/pages/PdfGenerator/components/Sidebar/SidebarElements.js
+++ src/pages/PdfGenerator/components/Sidebar/SidebarElements.js
@@ -8,9 +8,41 @@ export const Wrapper = styled.aside`
   padding: 1rem;
 `;
 
-export const Select = styled.select``;
+export const Select = styled.select`
+  // A reset of styles, including removing the default dropdown arrow
+  appearance: none;
+  // Additional resets for further consistency
+  background-color: transparent;
+  border: none;
+  padding: 0 1em 0 0;
+  margin: 0;
+  width: 100%;
+  font-family: inherit;
+  font-size: inherit;
+  cursor: inherit;
+  line-height: inherit;
+  ::-ms-expand {
+    display: none;
+  }
+  outline: none;
+  width: 100%;
+  border: 1px solid #9da192;
+  border-radius: 0.25em;
+  padding: 0.8rem;
+  font-size: 1rem;
+  cursor: pointer;
+  line-height: 1.1;
+`;
 
 export const Template = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const Sections = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+export const Postage = styled.div`
   display: flex;
   flex-direction: column;
 `;
@@ -18,4 +50,10 @@ export const Template = styled.div`
 export const Label = styled.label`
   font-weight: 600;
   font-size: 1.125rem;
+`;
+
+export const Message = styled.div`
+  padding: 1rem;
+  border-radius: 7px;
+  background-color: ;
 `;
